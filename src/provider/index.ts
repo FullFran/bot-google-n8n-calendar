b6f95b1d3fbe81cc84c2de2a55f82a88a6722ab1
Francisco Manuel Olmedo Cortés <128989182+FullFran@users.noreply.github.com>
2024-08-26 21:10:54 +0200

    Update index.ts

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -2,9 +2,9 @@ import { createProvider } from '@builderbot/bot';
 import { TelegramProvider } from '@builderbot/provider-telegram'; // Importa el proveedor de Telegram
 
 // Configura el proveedor de Telegram
-const telegramProvider = createProvider(TelegramProvider, {
+const provider = createProvider(TelegramProvider, {
   botToken: process.env.TELEGRAM_BOT_TOKEN,  // Asegúrate de tener el token del bot en tus variables de entorno
 });
 
 // Exporta ambos proveedores
-export telegramProvider;
+export provider;
